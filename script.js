e3c38b6f834c7a920f79f6861207db2d7f608b70
Russell Salcido <russell.salcido@gmail.com>
2020-05-06 20:02:24 -0700

    Connected prompt to button. Prompt asks how many chars.

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+document.getElementById("generate").onclick = prompt(
+  "Congratulations! You are one step closer to securing your very future!\nPlease choose how many characters you would like to generate.\n\nIn other words, how long do you need the password to be?\n\nChoose\nNo less than 8\nNo more than 128\n"
+);
+
+
+if(1){
+
+}
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+
+// WHEN prompted for character types to include in the pa  ssword
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
